7c185b8028d704000ff5178ea7a711de2237b1e0
Tim Watkins <timothy.watkins@gmail.com>
2018-01-30 18:46:05 -0700

    Update and rename config.js.sample to config.js

--- config/config.js
+++ config/config.js
@@ -9,20 +9,20 @@
  */
 
 var config = {
-	address: "localhost", // Address to listen on, can be:
+	address: "0.0.0.0", // Address to listen on, can be:
 	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 	                      // - another specific IPv4/6 to listen on a specific interface
 	                      // - "", "0.0.0.0", "::" to listen on any interface
 	                      // Default, when address config is left out, is "localhost"
 	port: 8080,
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "192.168.10.164"], // Set [] to allow all IP addresses
 	                                                       // or add a specific IPv4 of 192.168.1.5 :
 	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
 	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	language: "en",
-	timeFormat: 24,
+	timeFormat: 12,
 	units: "metric",
 
 	modules: [
@@ -39,28 +39,39 @@ var config = {
 		},
 		{
 			module: "calendar",
-			header: "US Holidays",
+			header: "Upcoming",
 			position: "top_left",
 			config: {
+				maximumEntries: 6,
+				maximumNumberOfDays: 30,
+				fadePoint: 0.5,
+				timeFormat: 'absolute',
+				getRelative: 0,
+				urgency: 0,
+				hidePrivate: true,
 				calendars: [
 					{
-						symbol: "calendar-check-o ",
-						url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
+						symbol: "calendar-check-o",
+						url: "https://calendar.google.com/calendar/ical/timothy.watkins%40gmail.com/private-389682ddeac193b3ddb26680a983c33f/basic.ics"
+					},
+					{
+						symbol: "users",
+						url: "https://calendar.google.com/calendar/ical/3l3qdvdr9g5t3dqreves9t5io8%40group.calendar.google.com/private-d7f47cea03df18a51b731a1b1e2da263/basic.ics"
 					}
 				]
 			}
 		},
 		{
 			module: "compliments",
-			position: "lower_third"
+			position: "bottom_center"
 		},
 		{
 			module: "currentweather",
 			position: "top_right",
 			config: {
-				location: "New York",
-				locationID: "",  //ID from http://www.openweathermap.org/help/city_list.txt
-				appid: "YOUR_OPENWEATHER_API_KEY"
+				locationID: "5913490",  //ID from http://www.openweathermap.org/help/city_list.txt
+				appid: "9d828133b53d04bbd381198abf9015c3",
+				units: "metric",
 			}
 		},
 		{
@@ -68,25 +79,19 @@ var config = {
 			position: "top_right",
 			header: "Weather Forecast",
 			config: {
-				location: "New York",
-				locationID: "5128581",  //ID from http://www.openweathermap.org/help/city_list.txt
-				appid: "YOUR_OPENWEATHER_API_KEY"
+				locationID: "5913490",  //ID from http://www.openweathermap.org/help/city_list.txt
+				appid: "9d828133b53d04bbd381198abf9015c3",
+				units: "metric",
 			}
 		},
 		{
-			module: "newsfeed",
-			position: "bottom_bar",
+			module: 'MMM-NHL',
+			position: 'top_right',
 			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true
+				focus_on: ['CGY'],
+				matches: 4
 			}
-		},
+		}
 	]
 
 };
