afbdb1b511a434cf3b04cf03670da472c4a1bca6
Tim Watkins <timothy.watkins@gmail.com>
2018-01-31 23:44:33 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -29,10 +29,10 @@ var config = {
 		{
 			module: "alert",
 		},
-		{
+		/*{
 			module: "updatenotification",
 			position: "top_bar"
-		},
+		},*/
 		{
 			module: "clock",
 			position: "top_left"
